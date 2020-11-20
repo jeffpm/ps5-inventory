9a266a51de35f7b69bf5ad7e9285d30cd32a9615
Jeff McElhannon <jeffpm@gmail.com>
2020-11-20 11:07:25 -0500

    first pass

---
+++ README.md
@@ -0,0 +1,23 @@
+1. install python 3.8 - https://www.python.org/downloads/release/python-386/
+2. install pipenv
+    `pip install pipenv`
+3. start a pipenv shell
+    `pipenv install --three`
+    `pipenv shell`
+4. OPTIONAL - configure things
+    - in a text editor, open inventory.py
+    - update `ZIP` and `RADIUS` values if desired
+    - update `WAIT` to the number of seconds between inventory fetches/notifications.
+5. OPTIONAL - configure twilio for sms notifications
+    - register a free twilio account - https://www.twilio.com/try-twilio
+    - once registered, create a trial number from https://www.twilio.com/console
+    - in inventory.py, update 
+        - `PHONE` with your real number
+        - `TRIAL_PHONE` with your twilio trial number
+        - `TWILIO-SID` with your twilio sid
+        - `TWILIO-TOKEN` with your twilio token
+6. run the damn thing
+    `python inventory.py`
+
+notes:
+yeah, the code sucks. i wrote this really quick. i might refactor it if i have time.
---
+++ inventory.py
@@ -0,0 +1,64 @@
+from lxml import etree
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import logging
+from twilio.rest import Client
+
+ZIP = "23238"
+SKU = "207-43-0001"
+RANGE = "25"
+WAIT = 10
+PHONE = "+1<your-phone>"
+TRIAL_PHONE = "+1<your-trial-phone>"
+TWILIO_SID = "<your-twilio-sid>"
+TWILIO_TOKEN = "<your-twilio-token>"
+
+def main():
+    logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.INFO, datefmt='%m/%d/%Y %I:%M:%S %p')
+
+    while True:
+        inventory = check_inventory()
+        if len(inventory) > 0:
+            notify(inventory)
+        else:
+            logging.info("no stock found")
+        sleep(WAIT)
+       
+def check_inventory():
+    available = []
+    url = f"https://popfindr.com/results?pid={SKU}&zip={ZIP}&range={RANGE}&webpage=target"
+    logging.info(f"checking: {url}")
+    r = requests.get(url)
+    html_string = r.text
+    soup = BeautifulSoup(html_string, 'lxml')
+    table = soup.find_all('table')[1]
+    if not table:
+        logging.info("site error, no inventory table returned")
+
+    for tr in table.find_all('tr')[1:]:
+        td = [th.get_text(strip=True) for th in tr.find_all('th')]
+        if int(td[2]) > 0:
+            available.append(td)
+    return available
+
+def notify(inventory):
+    print("STOCK FOUND:")
+    for item in inventory:
+        print(item)
+
+    account_sid = TWILIO_SID
+    auth_token = TWILIO_TOKEN
+    client = Client(account_sid, auth_token)
+    try:
+        message = client.messages \
+            .create(
+                    body="PS5 STOCK FOUND.",
+                    from_=TRIAL_PHONE,
+                    to=PHONE
+                )
+    except:
+        logging.info("couldn't send twilio message")
+
+if __name__ == "__main__":
+    main()
